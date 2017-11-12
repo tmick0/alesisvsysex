1d06a756fd5d476fd57918586847f47489b92918
lq0000 <root@lo.calho.st>
2017-11-12 11:21:37 -0700

    Add hooks for updating model on UI action

--- alesisvsysex/ui/window.py
+++ alesisvsysex/ui/window.py
@@ -62,9 +62,13 @@ class IntegerSelector (QSpinBox):
         self.setRange(0x00, 0x7f)
         self.setSingleStep(1)
         self.updateState()
+        self.valueChanged.connect(self.updateModel)
         
     def updateState(self):
         self.setValue(getattr(self.componentModel, self.fieldName).as_int())
+    
+    def updateModel(self):
+        setattr(self.componentModel, self.fieldName, IntValue(self.value()))
 
 class EnumSelector (QComboBox):
 
@@ -72,10 +76,12 @@ class EnumSelector (QComboBox):
         super().__init__(parent)
         self.fieldName = field
         self.componentModel = model
-        self.enumValues = list(sorted(model._params[field]._VALUES.items(), key=lambda x: x[1]))
+        self.enumClass = model._params[field].__class__
+        self.enumValues = list(sorted(self.enumClass._VALUES.items(), key=lambda x: x[1]))
         for k, v in self.enumValues:
             self.addItem(k, v)
         self.updateState()
+        self.currentIndexChanged.connect(self.updateModel)
         
     def updateState(self):
         for i, (k, v) in enumerate(self.enumValues):
@@ -85,6 +91,9 @@ class EnumSelector (QComboBox):
         else:
             raise RuntimeError("Invalid state for component '%s' field '%s'"
                                % (self.componentModel.__class__.__name__, self.fieldName))
+                               
+    def updateModel(self):
+        val = self.enumClass(self.enumValues[self.currentIndex()][1])
 
 class BasicWidget (QGroupBox):
     
